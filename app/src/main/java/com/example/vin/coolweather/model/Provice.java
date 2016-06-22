797d7d8c24cdb74d9014ebbccda061009fe387e3
vin-devel <wdyangll@126.com>
2016-06-22 18:15:20 +0800

    create db and model

---
+++ app/src/main/java/com/example/vin/coolweather/model/Provice.java
@@ -0,0 +1,34 @@
+package com.example.vin.coolweather.model;
+
+/**
+ * Created by vin on 16/6/22.
+ */
+public class Provice {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
